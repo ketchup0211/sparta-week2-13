48247284de15377b5c9de87ded61c1048bf03f80
t2023-m0052 <t2023-m0052@t2023-m0052ui-MacBookAir.local>
2024-01-12 11:07:02 +0900

    [UPDATE] 1. getMovieData from movieAPI.js / 2. filter movie card by searchkeword / 3. create searchMovieList

--- src/search-page.js
+++ src/search-page.js
@@ -1,8 +1,41 @@
-// API TMDB API data 가져오기
-// (1) nowlist 3개 가져오기 => 전체 영화를 sorting
-// (2 -1) movie id or title로 정렬 => 하나의 배열로 만들기?
-// (2- 2) TMDB search movie api를 가져와서 해보기 => test 필요함
-// (3) 검색어에 title이 포함되는 영화만 카드로 보여주기
+// (1) API TMDB API data - list 3개 가져오기 가져오기
+getMovieData();
+
+// (2) main page (index.html)에서 검색어 입력 시 search-page로 이동
+
+// (3) 검색어에 title이 포함된 영화만 filtering
+
+const filter = () => {
+  const searchElement = document.getElementById("search"); // input 태그
+  const searchElementInputValue = searchElement.value.toLowerCase();
+  const listInner = document.getElementsByClassName("searched-list");
+
+  for (let i = 0; i < listInner.length; i++) {
+    let searchTitle = listInner[i].querySelector(".movie-title");
+
+    if (searchTitle.textContent.toLowerCase().includes(searchElementInputValue)) {
+      listInner[i].style.display = "block";
+    } else {
+      listInner[i].style.display = "none";
+    }
+  }
+};
+
+// (4) searchMovieList를 바탕으로 카드 생성
+const searchMovielist = async () => {
+  const movies = await getMovieData();
+
+  const searchedList = document.querySelector("searched-list");
+  searchedList.innerHTML = movies
+    .map(
+      (movie) => `
+      <li class="movie-card" id=${movie.id}>
+      <img class="movie-img" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="...">
+      </li>
+    `
+    )
+    .join("");
+};
 
 // const showSearchResult = (searchKeyword) => {
 //   const movieCards = document.querySelectorAll(".movie-card");
